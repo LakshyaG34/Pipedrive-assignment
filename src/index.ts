6e2534b5aac0eadeb76603aff67720055cbdec6b
Lakshya Goyal <lakshyagoyal34@gmail.com>
2025-08-20 15:42:30 +0530

    Second Commit

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,8 @@ import type { PipedrivePerson } from "./types/pipedrive";
 import inputData from "./mappings/inputData.json";
 import mappings from "./mappings/mappings.json";
 
-// Load environment variables from .env file
 dotenv.config();
 
-// Get API key and company domain from environment variables
 const apiKey = process.env.PIPEDRIVE_API_KEY;
 const companyDomain = process.env.PIPEDRIVE_COMPANY_DOMAIN;
 
@@ -19,9 +17,13 @@ const syncPdPerson = async (): Promise<PipedrivePerson> => {
     }
 
     const payload: Record<string, any> = {};
+    function nestingResolve(obj: any, path: string):any {
+      return path.split(".").reduce((acc, key)=>acc?.[key], obj);
+    }
+
     for(const map of mappings as {pipedriveKey: string, inputKey: string}[])
     {
-      payload[map.pipedriveKey] = (inputData as any)[map.inputKey]
+      payload[map.pipedriveKey] = nestingResolve(inputData, map.inputKey);
     }
 
     const personName = payload["name"];
@@ -29,6 +31,15 @@ const syncPdPerson = async (): Promise<PipedrivePerson> => {
     {
       throw new Error("Name field Missing in payload");
     }
+    if(payload.email)
+    {
+      payload.email = [{value: payload.email, primary: true}];
+    }
+    if(payload.phone)
+    {
+      payload.phone = [{value: payload.phone, primary: true}];
+    }
+    // console.log("Payload:", JSON.stringify(payload, null, 2));
 
     const searchURL = `https://${companyDomain}.pipedrive.com/v1/persons/search?term=${encodeURIComponent(
       personName
@@ -41,7 +52,7 @@ const syncPdPerson = async (): Promise<PipedrivePerson> => {
 
     if(searchData?.data?.items?.length>0)
     {
-      const personId = searchData.data.item[0].item.id;
+      const personId = searchData.data.items[0].item.id;
       const updateURL = `https://${companyDomain}.pipedrive.com/v1/persons/${personId}?api_token=${apiKey}`
       const updateRes = await fetch(updateURL,{
         method: "PUT",
